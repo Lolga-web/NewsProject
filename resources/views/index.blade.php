768364565863cc9ac107c4a37131d04262661f5a
Lolga-web <lolga.web@mail.ru>
2021-03-31 16:57:43 +1000

    main page add last news

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,9 +7,50 @@
     
     <div class="main_page_container">
         <div class="main_section">
-            <div class="last_news">
-                
+            <div class="last_news_list">
+                @forelse($newNews as $item)
+                    @if ($loop->first)
+                        <a class="last_news_item_link" href="{{ route('news.show', $item) }}">
+                            <div class="last_news_item">
+                                <div class="last_news_item_img" style="background-image: url({{ $item->image ?? asset('storage/default.jpeg') }})"></div>
+                                
+                                <h2 class="last_news_item_title_first">
+                                    <div class="last_news_item_date">
+                                        @if (date('d-m-Y', strtotime($item->date)) == date('d-m-Y'))
+                                            {{ date('H:i', strtotime($item->date)) }} 
+                                        @else
+                                            {{ date('d-m-Y', strtotime($item->date)) }}
+                                        @endif
+                                    </div>
+                                    {{ $item->title }}
+                                </h2>
+                            </div>
+                        </a>
+                    @else
+                    <a class="last_news_item_link" href="{{ route('news.show', $item) }}">
+                        <div class="last_news_item">
+                            <h2 class="last_news_item_title">
+                                <div class="last_news_item_date">
+                                    @if (date('d-m-Y', strtotime($item->date)) == date('d-m-Y'))
+                                        {{ date('H:i', strtotime($item->date)) }} 
+                                    @else
+                                        {{ date('d-m-Y', strtotime($item->date)) }}
+                                    @endif
+                                </div>
+                                {{ $item->title }}
+                            </h2>
+                        </div>
+                    </a>
+                    @endif
+                    
+                @empty
+                    <p>Нет новостей</p>
+                @endforelse   
+                <a class="news_in_category_btn" href="{{ route('news.index') }}">
+                    Больше новостей
+                </a>              
             </div>
+            
         </div>
 
         <div class="right_section">
